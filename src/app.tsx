65142f85e8f67033798b7edfe0a24405bc109062
Krb0 <maurodavidcaceres@gmail.com>
2023-01-13 23:29:36 -0300

    build: :rocket: initial setup

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import './app.css'
+import { io } from 'socket.io-client'
+const socket = io()
+export function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
